58989937690a21c381d89fa9986ddabdd2c47e8d
silentruntime <silentruntime@protonmail.com>
2025-03-31 14:39:00 -0400

    page transitions

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ const totalImages = document.querySelectorAll("img").length
 let curImageDisplay;
 const modal = document.querySelector(".overlay")
 
+const pageTransition = () => {
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        let href = this.getAttribute('href');
+  
+        document.body.classList.add('fade-out');
+  
+        setTimeout(() => {
+          window.location.href = href;
+        }, 500); // Duration matches the CSS transition
+      });
+    });
+  };
+  
+  document.addEventListener('DOMContentLoaded', pageTransition);
+
 document.addEventListener("keydown", (e) => {
     //load text
     console.log(e.code)
