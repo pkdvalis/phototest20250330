e702913eb9a9cd33f157233ffba12a55d646f40b
silentruntime <silentruntime@protonmail.com>
2025-04-01 10:07:08 -0400

    mobile swipe

--- script.js
+++ script.js
@@ -191,3 +191,66 @@ for (let i = 0; i < totalImages; i++) {
     .replace(/webp/g, "jpg");
   console.log("displayiamges", displayImages[i]);
 }
+
+
+//mobile swipe
+var initialTouchX, initialTouchY,
+ finalTouchX, finalTouchY;
+var swipeThreshold = 100; 
+var dynamicStyle = 
+document.createElement("style");
+document.body.
+appendChild(dynamicStyle);
+
+
+function handleTouch(startX, endX,
+     onSwipeLeft, onSwipeRight) {
+    var horizontalDistance = 
+    finalTouchX - initialTouchX;
+    var verticalDistance = 
+    finalTouchY - initialTouchY;
+
+    if (Math.abs(horizontalDistance) >
+     Math.abs(verticalDistance) &&
+      Math.abs(horizontalDistance) >
+       swipeThreshold) {
+        if (finalTouchX - 
+            initialTouchX < 0) {
+            onSwipeLeft(); 
+        } else {
+            onSwipeRight(); 
+        }
+    }
+}
+
+
+var swipeLeft = () => {
+    prevImage()
+};
+
+var swipeRight = () => {
+    nextImageImage()
+};
+
+
+window.onload = function () {
+    window.addEventListener
+    ('touchstart', function (event) {
+        initialTouchX = 
+        event.touches[0].clientX;
+        initialTouchY =
+         event.touches[0].clientY;
+    });
+
+    window.addEventListener
+    ('touchend', function (event) {
+        finalTouchX = event.
+        changedTouches[0].clientX;
+        finalTouchY = event.
+        changedTouches[0].clientY;
+
+        
+        handleTouch(initialTouchX,
+        finalTouchX, swipeLeft, swipeRight);
+    });
+};
